245daaa3b55c51d513ce8eb09b642b4dc232a7c9
Matt Campbell <matthewricecampbell@gmail.com>
2017-06-21 11:02:22 -0400

    Add jwt

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ Bundler.require(*Rails.groups)
 
 module RailsApiUserAuthBoilerplate
   class Application < Rails::Application
+    config.autoload_paths << Rails.root.join('lib')
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
